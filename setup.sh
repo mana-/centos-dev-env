d4a0c75b19080cea0fc34b93d5908a2fa999b669
manabu <dev-02@vagrant-centos65.vagrantup.com>
2016-10-14 19:00:47 +0900

    設定ファイルを配置するようにした

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# 設定ファイルの配置
+if [ -e ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.bak
+fi
+ln -s ~/centos-dev-env/.vimrc ~/.vimrc
+
+if [ -e ~/.bashrc ]; then
+  mv ~/.bashrc ~/.bashrc.bak  
+fi
+ln -s ~/centos-dev-env/.bashrc ~/.bashrc
+
 # vim
 mkdir -p ~/.vim/bundle
-if [ -e ~/.vim/bundle/neobundle.vim ]; then
-  # ファイルの存在あり
-  echo "neobundle already installed."
-else
+if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
   git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
-
 
